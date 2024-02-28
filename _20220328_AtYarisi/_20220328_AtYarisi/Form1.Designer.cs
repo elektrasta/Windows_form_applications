83bce88166892d90a16c119398fb6309bbe4c3b9
Ömer Faruk Çetinkaya <121095446+elektrasta@users.noreply.github.com>
2024-02-29 01:20:30 +0300

    update at yarisi

---
+++ _20220328_AtYarisi/_20220328_AtYarisi/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace _20220328_AtYarisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbAt1 = new System.Windows.Forms.PictureBox();
+            this.pbAt2 = new System.Windows.Forms.PictureBox();
+            this.pbAt3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.pnlAt1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAt1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAt2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAt3)).BeginInit();
+            this.pnlAt1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbAt1
+            // 
+            this.pbAt1.Image = ((System.Drawing.Image)(resources.GetObject("pbAt1.Image")));
+            this.pbAt1.Location = new System.Drawing.Point(3, 3);
+            this.pbAt1.Name = "pbAt1";
+            this.pbAt1.Size = new System.Drawing.Size(174, 137);
+            this.pbAt1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbAt1.TabIndex = 0;
+            this.pbAt1.TabStop = false;
+            // 
+            // pbAt2
+            // 
+            this.pbAt2.Image = ((System.Drawing.Image)(resources.GetObject("pbAt2.Image")));
+            this.pbAt2.Location = new System.Drawing.Point(44, 195);
+            this.pbAt2.Name = "pbAt2";
+            this.pbAt2.Size = new System.Drawing.Size(177, 137);
+            this.pbAt2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbAt2.TabIndex = 1;
+            this.pbAt2.TabStop = false;
+            // 
+            // pbAt3
+            // 
+            this.pbAt3.Image = ((System.Drawing.Image)(resources.GetObject("pbAt3.Image")));
+            this.pbAt3.Location = new System.Drawing.Point(44, 354);
+            this.pbAt3.Name = "pbAt3";
+            this.pbAt3.Size = new System.Drawing.Size(177, 137);
+            this.pbAt3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbAt3.TabIndex = 2;
+            this.pbAt3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBaslat.Location = new System.Drawing.Point(44, 558);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(177, 57);
+            this.btnBaslat.TabIndex = 3;
+            this.btnBaslat.Text = "Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // pnlAt1
+            // 
+            this.pnlAt1.Controls.Add(this.label1);
+            this.pnlAt1.Controls.Add(this.pbAt1);
+            this.pnlAt1.Location = new System.Drawing.Point(44, 12);
+            this.pnlAt1.Name = "pnlAt1";
+            this.pnlAt1.Size = new System.Drawing.Size(177, 188);
+            this.pnlAt1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(22, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bold Pilot";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1205, 627);
+            this.Controls.Add(this.pnlAt1);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.pbAt3);
+            this.Controls.Add(this.pbAt2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbAt1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAt2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAt3)).EndInit();
+            this.pnlAt1.ResumeLayout(false);
+            this.pnlAt1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbAt1;
+        private System.Windows.Forms.PictureBox pbAt2;
+        private System.Windows.Forms.PictureBox pbAt3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Panel pnlAt1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ _20220328_AtYarisi/_20220328_AtYarisi/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20220328_AtYarisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            Point at1 = pnlAt1.Location;
+            at1.X = at1.X + r.Next(5,20);
+            //if(at1.X<300)
+            //    at1.X = at1.X + r.Next(4, 15);
+            //else
+            //    at1.X = at1.X + r.Next(14, 20);
+            pnlAt1.Location = at1;
+
+            Point at2 = pbAt2.Location;
+            at2.X = at2.X + r.Next(5, 20);
+            pbAt2.Location = at2;
+
+            Point at3 = pbAt3.Location;
+            at3.X = at3.X + r.Next(5, 20);
+            pbAt3.Location = at3;
+
+            int durmaNoktasi = 1000;
+
+            // Durma şartı ? 800 piksel
+            if(at1.X>= durmaNoktasi)
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("1. At kazandı");
+            }
+            else if (at2.X >= durmaNoktasi)
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("2. At kazandı");
+            }
+            else if (at3.X >= durmaNoktasi)
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("3. At kazandı");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //timer1.Enabled = true;
+        }
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == false)
+            {
+                timer1.Enabled = true;
+                btnBaslat.Text = "Durdur";
+            }     
+            else
+            {
+                timer1.Enabled = false;
+                btnBaslat.Text = "Başlat";
+            }
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    Point at1 = pbAt1.Location;
+        //    at1.X = at1.X + 20;
+        //    pbAt1.Location = at1;
+        //}
+    }
+}
